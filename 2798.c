1e7b35d4f7d6462fa375d0c48cf9857ed34d774d
김정규 <junggkim@gimjeong-gyus-MacBook-Pro.local>
2023-01-01 19:38:24 +0900

    dkdkd12

--- 2798.c
+++ 2798.c
@@ -1,46 +1,34 @@
 #include <stdio.h>
 
-int	check_card(char card[101], int N, int M)
+int	main(void)
 {
-	int	i;
-	int	result;
-	char	sum[];
+	int	N,M,i,j,k;
+	int	card[100] = { 0, };
+	int	result,max = 0;
 
+	scanf("%d %d",&N,&M);
 	i = 0;
-	result = 0;
-	while (1)
-	{
-		if (card[i + 2] == NULL)
-			break ;
-		sum[i] = card[i] + card[i + 1] + card[i + 2] 
-		if (sum[i] == M)
-			return (sum[i]);
-			break;
-		else if (sum[i] > M)
-			break ;
-		ddddd
-
-
-
-
-}
-
-int	main()
-{
-	int	N,M,i;
-	char	card[101];
-	int	result;
-
-	scanf("%d %d\n",&N,&M);
-	i = 0;
+	j = 0;
+	k = 0;
 	while(i < N)
+		scanf("%d",&card[i++]);
+	i = 0;
+	while (i < N)
 	{
-		scanf("%d",card[i]);
-		if (card[i] > M)
-			return (0);
+		j = i + 1;
+		while(j < N)
+		{
+			k = j + 1;
+			while(k < N)
+			{
+				max = card[i] + card[j] + card[k];
+				if (max > result && max <= M)
+					result = max;
+				k++;
+			}
+			j++;
+		}
 		i++;
 	}
-	result = check_card(card[101],N,M);
 	printf("%d",result);
-	return (0);
 }

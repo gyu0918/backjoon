70c5d10be74e5b008a0090d900e0fe46362ecd21
gyu0918 <kjk1526@naver.com>
2023-01-07 18:44:39 +0900

    17

---
+++ 1018.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int	main()
+{
+	int	i, j, N, M, x, y;
+	int	count;
+	char	board[50][50];
+	int	temp1;
+	int	temp2;
+
+	scanf("%d %d\n", &N, &M);
+	i = 0;
+	while (i < N)
+	{
+		scanf("%s", board[i]);
+		i++;
+	}
+	temp1 = 0;
+	temp2 = 0;
+	x = 0;
+	y = 0;
+	while (
+	
+	
+
+	return (0);
+}

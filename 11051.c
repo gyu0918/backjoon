916d3c074beac9dbe4844975a107002aa81bbd52
gyu0918 <kjk1526@naver.com>
2023-02-03 23:09:06 +0900

    backjoonss

--- 11051.c
+++ 11051.c
@@ -1,31 +1,42 @@
-#include <stdio.h>
+}
+ 15         return (result);
+ 16 }
+ 17
+ 18 int     main()
+ 19 {
+ 20         int     N, K;
+ 21         int     result;
+ 22         int     A, B, C;
+ 23
+ 2#include <stdio.h>
+#define mod 10007
+#define MAX 1001
 
-int	Factorial(int num)
+int dp[MAX][MAX];
+
+void fibo(int n, int k)
 {
-	int	i;
-	int	result;
-
-	result = 1;
-	i = 1;
-	while (i <= num)
-	{
-		result *= i;
-		i++;
-	}		
-	return (result);
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= k; j++)
+        {
+            if (i == j || j == 0)
+            {
+                dp[i][j] = 1;
+            }
+            else
+            {
+                dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % mod;
+            }
+        }
+    }
 }
 
-int	main()
+int main()
 {
-	int	N, K;
-	int	result;
-	int	A, B, C;
-
-	scanf("%d %d", &N, &K);
-	A = Factorial(N);
-	B = Factorial(K);
-	C = Factorial(N - K);
-	result = A/(B*C);
-	printf("%d\n", result%10007);
-	return (0);
+    int n, k;
+    scanf("%d%d", &n, &k);
+    fibo(n, k);
+    printf("%d\n", dp[n][k]);
+    return 0;
 }

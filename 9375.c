5157a4d2bfbf4cee6db52d3f13aa809183479929
gyu0918 <kjk1526@naver.com>
2023-02-05 23:01:31 +0900

    Dddddddddd

---
+++ 9375.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int	main()
+{
+    int     T, N;
+    char    result[101][31] = {0};
+    int     i;
+
+    scanf("%d", &T);
+    while (T--)
+    {
+        scanf("%d", &N);
+        i = 0;
+        while (i < N)
+        {
+            scanf("%s %s" result[i][0], result[i][1]);
+            i++;
+        }
+        
+    }
+    
+
+
+
+
+    return (0);
+}
+
+
+
+
+
+
---
+++ test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	char	result[4]={0};
+
+	scanf("%s", result);
+	printf("%s", result);
+}

5d45811af97bdbc49b40eee517c0f1c15543a467
gyu0918 <kjk1526@naver.com>
2023-02-10 14:27:26 +0900

    backjoon2222

---
+++ 14888.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int n;
+int min = 1000000001;
+int max = -1000000001;
+int cal[12];
+void DFS(int plus, int minus, int mult, int div, int x, int sum)
+{
+	if (x == n - 1)
+	{
+		if (max < sum)
+			max = sum;
+		if (min > sum)
+			min = sum;
+	}
+	if (plus > 0)
+		DFS(plus - 1, minus, mult, div, x + 1, sum + cal[x + 1]);
+	if (minus > 0)
+		DFS(plus, minus - 1, mult, div, x + 1, sum - cal[x + 1]);
+	if (mult > 0)
+		DFS(plus, minus, mult - 1, div, x + 1, sum * cal[x + 1]);
+	if (div > 0)
+		DFS(plus, minus, mult, div - 1, x + 1, sum / cal[x + 1]);
+}
+
+int main()
+{
+	int plus, minus, mult, div, i;
+
+	scanf("%d", &n);
+	i = 0;
+	while (i < n)
+	{
+		scanf("%d", &cal[i]);
+		i++;
+	}
+	scanf("%d %d %d %d", &plus, &minus, &mult, &div);
+	DFS(plus, minus, mult, div, 0, cal[0]);
+	printf("%d\n%d", max, min);
+}

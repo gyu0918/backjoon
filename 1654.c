3868648b033011221113813b22ca80787bf8d54f
gyu0918 <111268088+gyu0918@users.noreply.github.com>
2023-03-07 22:51:05 +0900

    Add files via upload

--- 1654.c
+++ 1654.c
@@ -3,7 +3,7 @@
 int main()
 {
     int         K, N, i, j;
-    long long   k_max = 0, max_len = 0, cnt;
+    long long   k_max = 0, max_len = 0, count;
     long long   arr[10001] = {0};
     long long   left, mid, right;
 
@@ -22,15 +22,15 @@ int main()
     {
         mid = (left + right) / 2;
         j = 0;
-        cnt = 0;
+        count = 0;
         while (j < K)
         {
-            cnt += arr[j] / mid;
+            count += arr[j] / mid;
             j++;
         }
-        if (N <= cnt && max_len < mid)
+        if (N <= count && max_len < mid)
             max_len = mid;
-        if (cnt < N)
+        if (count < N)
             right = mid - 1;
         else
             left = mid + 1;       

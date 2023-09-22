bdc5c8c781729d7b9d46cb31da0d7e440cdcddc1
gyu0918 <kjk1526@naver.com>
2023-09-22 23:09:19 +0900

    back

--- javaBackjoon/src/Back2468.java
+++ javaBackjoon/src/Back2468.java
@@ -1,23 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.SequenceInputStream;
 import java.util.StringTokenizer;
+import java.util.logging.SocketHandler;
 
-public class Test {
+public class Back2468 {
     static int n;
-    static boolean  checked[][];
+    static int count;
     static int  map[][];
-    static int  dx[] = {-1, 0, 1, 0};
-    static int  dy[] = {0, 1, 0, -1};
+    static boolean  checked[][];
+    static int  dx[] = {1, 0, -1, 0};
+    static int  dy[] = {0, -1, 0, 1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         map = new int[n][n];
 
         int maxHeight = 0;
-        for (int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
@@ -32,24 +33,25 @@ public class Test {
             int cnt = 0;
             for (int i = 0; i < n; i++){
                 for (int j = 0; j < n; j++){
-                    if (!checked[i][j] && map[i][j] > height)
+                    if (!checked[i][j] && map[i][j] > height){
                         cnt += dfs(i, j, height);
+                    }
                 }
             }
             max = Math.max(max, cnt);
         }
         System.out.println(max);
     }
-
-    static int dfs(int x, int y, int height){
+    static int  dfs(int x, int y, int height){
         checked[x][y] = true;
         for (int i = 0; i < 4; i++){
             int nx = x + dx[i];
             int ny = y + dy[i];
             if (nx < 0 || ny < 0 || nx > n - 1 || ny > n - 1) continue;
             if (checked[nx][ny]) continue;
-            if (map[nx][ny] > height)
+            if (map[nx][ny] > height){
                 dfs(nx, ny, height);
+            }
         }
         return (1);
     }

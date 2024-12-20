8cbc869c2a8034b9d4ce1cd6829401de19708121
김정규 <111268088+gyu0918@users.noreply.github.com>
2024-12-21 00:36:55 +0900

    back2720Second

--- back2720/src/main/java/org/example/Main.java
+++ back2720/src/main/java/org/example/Main.java
@@ -9,22 +9,28 @@ import java.io.*;
 
 public class Main {
     static int q = 25, d = 10, n = 5, p = 1;
-    static int qq,dd,nn,pp;
+    static int countq, countd, countn, countp;
     static int c;
 
     public static void main(String[] args) throws IOException {
         BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(rd.readLine());
         for (int i = 0; i < T; i++){
-            qq = 0;
             int c = Integer.parseInt(rd.readLine());
-            for (int j = 1; c < q * j; j++){
-                if (c == q * j) {
-                    qq = j;
-                    break;
-                }
+
+            if (c < 1 || c > 500) { // 입력값 범위 검증
+                throw new IllegalArgumentException("c must be between 1 and 500");
             }
 
+            countq = c /q;
+            c = c % q;
+            countd = c / d;
+            c = c % d;
+            countn = c / n;
+            c = c % n;
+            countp = c / p;
+            c = c % p;
+            System.out.println(countq + " " + countd + " " + countn + " " + countp);
         }
     }
 }
